461b2db598e02ec62b3d1662b244412c5c5efafa
JennyTan123 <85146823+JennyTan123@users.noreply.github.com>
2023-03-08 23:19:59 +0800

    Third commit

--- GA.py
+++ GA.py
@@ -4,7 +4,7 @@ import numpy as np
 
 class GA:
     random.seed(42)
-    def __init__(self,MAX_GENERATION,POP_SIZE,MAX_SAME_ANSWER_PRODUCED,CROSS_RATE,GEN_MUT_RATE,sc):
+    def __init__(self,MAX_GENERATION,POP_SIZE,MAX_SAME_ANSWER_PRODUCED,CROSS_RATE,GEN_MUT_RATE,sc:SupplyChain):
         self.MAX_GENERATION=MAX_GENERATION
         self.POP_SIZE=POP_SIZE
         self.MAX_SAME_ANSWER_PRODUCED=MAX_SAME_ANSWER_PRODUCED
@@ -19,12 +19,13 @@ class GA:
         '''Step 1: Initiating first population'''
         def generateChromosome():
             '''Generating chromosomes based on population size'''
-            chromosome=[random.randint(1,sc.NO_OF_DC) for i in range(sc.NO_OF_RETAILER)]
+            #NO_OF_DC
+            chromosome=[random.randint(1,sc.NO_OF_DC+1) for i in range(sc.NO_OF_RETAILER)]
             return chromosome
         return [generateChromosome() for _ in range(self.POP_SIZE)]
         
     #=======================================================================================================
-    def evaluateChromosome(self):
+    def evaluateChromosome(self): #Maybe put in run
         '''Step 2: Evaluating chromosomes: Using fitness function (Min TC form SC)'''
         #self.sc.calcTotalCost(X,Y,s,Q)
         #In population then loop for each chromosome, then find the fitness
@@ -58,6 +59,42 @@ class GA:
         pass
 
     #=======================================================================================================
+    def calcOptimalSQ(self,X,Y):
+        '''Step 2: Find optimal S0j, Q0j and costs'''
+        #Call step 1
+        Qmin=self.sc.calcQmin()
+
+        #Step 2:Calculate Q0 and s0 to find optimal Q0 and s0
+        #Loop for every DC's s0 and q0
+        s0List=[]
+        q0List=[]
+        for j in range(self.sc.NO_OF_DC):#Loop every DC to calculate
+            breakQFlag=False #If found correct then break
+            minValue=9999 #In case cannot found=0, then found the nearest min value
+            targetS0=None 
+            targetQ0=None
+            for q0 in reversed(range(2,round(Qmin[j]))): #Loop q0, starting from q1-1 reversed=1259
+                #For every q0 test for every s0
+                for s0 in range(1,q0):#X only take one column try 1-1258
+                    diff=self.sc.calcTotalCostForEachDC(X[j],Y[j],s0+1,q0)-self.sc.calcTotalCostForEachDC(X[j],Y[j],s0,q0)
+                    if diff<minValue:
+                        minValue=diff
+                        targetS0=s0
+                        targetQ0=q0
+                    elif diff==0:
+                        minValue=0
+                        s0List.append(s0)
+                        q0List.append(q0)
+                        breakQFlag=True
+                        break
+                if breakQFlag==True:
+                    break
+            #If cannot cater 0
+            if minValue!=0:
+                s0List.append(targetS0)
+                q0List.append(targetQ0)
+           
+                
     def run(self):
         '''Function to run the GA'''
         #Step 1: initialization
