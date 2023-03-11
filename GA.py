6e4a2a97bfe8085589e4311124308b830fa729f9
JennyTan123 <85146823+JennyTan123@users.noreply.github.com>
2023-03-11 12:54:08 +0800

    11/03

--- GA.py
+++ GA.py
@@ -1,6 +1,7 @@
 from SupplyChain import SupplyChain 
 import random
 import numpy as np
+import math
 
 class GA:
     random.seed(42)
@@ -14,14 +15,29 @@ class GA:
         self.INDIVIDUAL_LENGTH=self.sc.NO_OF_RETAILER
         #TODO rmb to change back NO_OF_RETAILER & DC
 
-    #=======================================================================================================
     def generateChromosome(self):
         '''Generating chromosomes based on the size of DC and retailer'''
         #NO_OF_DC+1 cater for the dummy DC
         chromosome=[random.randint(1,sc.NO_OF_DC+1) for i in range(sc.NO_OF_RETAILER)]
         return chromosome
+    
+    def generateX(self,chromosome:list):
+        '''Matrix X is generated for each chromosome'''
+        X=np.zeros((self.NO_OF_RETAILER,self.NO_OF_DC)).astype(int)
+        for idx,dc in enumerate(chromosome):
+            X[idx][dc-1]=1
+        return X
+      
+    def generateY(self,X):
+        '''Matrix Y is generated based on X'''
+        Y=np.zeros(self.NO_OF_DC).astype(int).tolist()
+        for j in range(self.NO_OF_DC):
+        #Loop each retailer whether it got assign to the DC 
+            for i in range(self.NO_OF_RETAILER):
+                if (X[i][j]==1):
+                    Y[j]=1 
+        return Y
         
-    #=======================================================================================================
     def evaluateChromosome(self,chromosome): 
         '''Evaluating chromosomes: Using fitness function (Min TC form SC)'''
         #Generate X and Y
@@ -29,38 +45,93 @@ class GA:
         Y=self.sc.generateY(X)
         #Compute optimum s and Q (a list with j elements) 
         finalOptimalSQ=self.calcOptimumSQ(X,Y)
+        #Check constraint, if not fulfill
+
         #Then pass to fitness:  #TODO check X or Y whether dummy DC is got open or not, if got add penalty cost
         fitness=self.sc.calcPenaltyCost(chromosome)+self.sc.calcTotalCost()
         return fitness
 
-    #=======================================================================================================
-    def elitism(self):
-        '''Retaining the best individuals in a gen unchanged in the next gen'''
-        pass
+    def rouletteWheelSelection(self,population):
+        population_fitness=sum([self.evaluateChromosome(population[ind]) for ind in range(len(population))])
+        #Compute each chromosome's probability 
+        chromosome_prob=[self.evaluateChromosome(chromosome)/population_fitness for chromosome in population]
+        #Making the prob for minimization prob
+        chromosome_prob=1-np.array(chromosome_prob)
+        chromosome_prob=chromosome_prob.tolist()
+        #Generate random r, if a current sum>r, then select that chromosome
+        return population[chromosome_prob.index(np.random.choice(chromosome_prob))]
 
-    #=======================================================================================================
-    def rouletteWheelSelection(self):
-        '''Parents selection based on roulette wheel selection'''
-        pass
+    def cross_over(self,parent1,parent2):
+        #Get the random number to split and perform crossover
+        pos=random.randrange(1,len(parent1)-1)
+        child1=parent1[:pos]+parent2[pos:]
+        child2=parent2[:pos]+parent1[pos:]
+        return child1,child2
 
-    #=======================================================================================================
-    def crossOver(self,n_parents):
-        '''Crossover 2 parents to generate new offspring'''
-        pass
-    #=======================================================================================================
-    def inversion_mutation(population,chromosome):
-        '''Mutation used to maintain the genetic diversity of the chromosomes'''
-        #     #Mutation operator in GA avoids convergence to local optimum and diverfies the population
-        #     #Replacing a gene with a randomly selected number according to the parameter's boundaries
-        pass
+    def mutation(self,chromosome):
+        #Generate a random number based on the range of NO_OF_DC, then iterate based on the mutation iteration
+        mutation_no=random.randint(1,self.sc.NO_OF_DC)
+        #Replace chromosome index with mutation no
+        replace_index=random.randint(0,self.INDIVIDUAL_LENGTH-1)
+        chromosome[replace_index]=mutation_no
+        return chromosome
+    
+    def newGeneration(self):
+        history={}
+        best_individual=(9999999,0)
+        #Step 1: Initiating first population
+        self.population=[self.generateChromosome() for _ in range(self.POP_SIZE)]
 
-    #=======================================================================================================
-    def evaluateOffsprings(self):
-        '''Evaluate the performance of offsprings'''
-        #Best chromosome, everytime will check whether it will better than best chromosome
-        pass
+        for iter in range(self.MAX_GENERATION):
+            ranked_population=[]
+            #Step 2: Evaluating chromosome
+            for chromosome in enumerate(population):
+                ranked_population.append((self.evaluateChromosome(chromosome[1]),chromosome[1]))
 
-    #=======================================================================================================
+            #Rank from low to high -> The lowest the better
+            ranked_population.sort()
+
+            #Compare the current population's best individual with the current best individual
+            if(ranked_population[0][0]<best_individual[0]):
+                best_individual=ranked_population[0]
+                
+            #Update history
+            history[iter]={'Best':ranked_population[0][0]}
+
+            #Step 3: Elitism - Select the top 20% from ranked_pop which use to remain for next generation
+            elitism_population=[ind[1] for ind in ranked_population[:round(POP_SIZE*0.2)]]
+
+            #Parent selection and crossover
+            n_parents=round(POP_SIZE*0.8)
+            #Make sure n_parents is in an even number
+            n_parents=(n_parents if n_parents%2==0 else n_parents-1)
+            new_population=[]
+            for i in range(0,n_parents,2):
+                if random.uniform(0,1)<CROSS_RATE:
+                    while(True):
+                        #Step 4: Parent selection - Select 80% from ranked_pop for parent selection
+                        parent1=self.rouletteWheelSelection(population)
+                        parent2=self.rouletteWheelSelection(population)
+                        if parent1!=parent2:
+                            break
+                    #Step 5: Crossover
+                    child1,child2=self.cross_over(parent1,parent2)
+                    new_population.append(child1)
+                    new_population.append(child2)
+                else:
+                    new_population.append(population[i])
+                    new_population.append(population[i+1])
+
+            #Step 6: Mutation - Undergo mutation process if the random generated no < 0.2
+            for idx,ind in enumerate(new_population):
+                #10% of genes from selected chromosomes for crossover are chosen
+                if random.uniform(0,1) < GEN_MUT_RATE:
+                    mut_iter=math.ceil(self.INDIVIDUAL_LENGTH*0.1)
+                    for i in range(mut_iter):
+                        new_population[idx]=self.mutation(ind)
+
+            population=elitism_population+new_population
+
     def calcOptimumSQ(self,X,Y):
         '''Find optimal S0j, Q0j and costs'''
         #-- Step1: Find Qmin for each DC --
@@ -119,44 +190,20 @@ class GA:
                 q1List.append(targetQ1)
         
         #-- Step 4: Compare the TC cost based on Step 2 and 3, find the optimal s and q --
-        finalOptimalSQ=[]
+        #finalOptimalSQ=[]
+        optimalS=[]
+        optimalQ=[]
         for j in range(self.sc.NO_OF_DC):
             tcCost1=self.sc.calcTotalCostForEachDC(X[j],Y[j],s0List[j],q0List[j])
             tcCost2=self.sc.calcTotalCostForEachDC(X[j],Y[j],0,q1List[j])
             if(tcCost1<tcCost2):
                 #Save as tuple
-                finalOptimalSQ.append((s0List[j],q0List[j]))
+                optimalS.append(s0List[j])
+                optimalQ.append(q0List[j])
             else:#tcCost1>tcCost2
-                finalOptimalSQ.append((0,q1List[j]))
-
-        return finalOptimalSQ
-           
-    def run(self):
-        '''Function to run the GA'''
-        #Step 1: Initiating first population
-        self.population=[self.generateChromosome() for _ in range(self.POP_SIZE)]
+                optimalS.append(s0List[j])
+                optimalQ.append(q0List[j])
         
-        for iter in range(self.MAX_GENERATION): #Total iterations
-            #Step 2: Evaluating chromosome
-            ranked_population=[]
-            for idx,chromosome in enumerate(self.population):
-                ranked_population.append(self.evaluateChromosome(chromosome))
-            
-            #Step 3: Reproduction (Elitism)
-            #Chromosomes with higher fitness values are more desirable
-            #For each generation, a constant percentage of the chromosomes with higher fitness value are automatically 
-            #copied to the next generation  
-            
-
-            #Step 4: Parents selection
-
-            #Step 5: Crossover
-
-            #Step 6: 
-            pass
-        
-        
-
 sc=SupplyChain('5-10','TYPE_I')
 MAX_GENERATION=10000
 POP_SIZE=20
@@ -164,6 +211,6 @@ MAX_SAME_ANSWER_PRODUCED=100
 CROSS_RATE=0.8
 GEN_MUT_RATE=0.2
 GA=GA(MAX_GENERATION,POP_SIZE,MAX_SAME_ANSWER_PRODUCED,CROSS_RATE,GEN_MUT_RATE,sc)
-GA.run()
+GA.newGeneration()
 
 
--- SupplyChain.py
+++ SupplyChain.py
@@ -3,32 +3,27 @@ import random
 import numpy as np
 
 class SupplyChain:
-
     def __init__(self,PROBLEM_SIZE,SITUATION_TYPE,display_param=False):
         random.seed(42)
+
         #Load yaml file
         with open('PARAMETERS_INITIALIZATION.yml','r') as f:
             PARAMETERS_INITIALIZATION=yaml.safe_load(f)
-        #======================================================================================================
+
         #Find problem size and initialize each number of Suppliers, DCs and Retailers
         try:
-            number_probSize=PARAMETERS_INITIALIZATION['PROBLEM_SIZE'][PROBLEM_SIZE]
+            number_probSize = PARAMETERS_INITIALIZATION['PROBLEM_SIZE'][PROBLEM_SIZE]
         except:
-            raise ValueError(f'Unknown Problem Size -{PROBLEM_SIZE}')
-        self.NO_OF_SUPPLIER,self.NO_OF_DC,self.NO_OF_RETAILER=number_probSize
-  
-        #======================================================================================================
-        #Find cost based on situation type    
+            raise ValueError(f"Unknown Problem Size - {PROBLEM_SIZE}")
+    
+        #Find cost based on situation type
         try:
-            cost_list=PARAMETERS_INITIALIZATION['SITUATION_TYPE'][cost_types]
+            cost_list = PARAMETERS_INITIALIZATION['SITUATION_TYPE'][SITUATION_TYPE]
         except:
-            raise ValueError(f'Unknown Situation Type -{SITUATION_TYPE}')            
-
-        #======================================================================================================        
+            raise ValueError(f"Unknown Situation Type - {SITUATION_TYPE}")
+        
         #Problem size initialization          
-        self.NO_OF_SUPPLIER=number_probSize[0]
-        self.NO_OF_DC=number_probSize[1]
-        self.NO_OF_RETAILER=number_probSize[2]
+        self.NO_OF_SUPPLIER, self.NO_OF_DC, self.NO_OF_RETAILER = number_probSize
 
         #Create a cost list, assign a random value based on the cost range of SITUATION_TYPE for each DC
         self.SETUP_COST=[random.randint(cost_list['SETUP_COST'][0],cost_list['SETUP_COST'][1]) for _ in range(self.NO_OF_DC)]
@@ -55,33 +50,33 @@ class SupplyChain:
                 self.RETAILER_ARRIVAL_RATE_1[i]=self.RETAILER_ARRIVAL_RATE_2[i]
                 self.RETAILER_ARRIVAL_RATE_2[i]=temp
 
-        if display_param==False:
-            display_param()
+        if display_param:
+            self.display_param()
 
     def display_param(self):
+        """Display parameters initialized."""
         print('Number of suppliers : ',self.NO_OF_SUPPLIER)
         print('Number of DCs       : ',self.NO_OF_DC)
         print('Number of retailers : ',self.NO_OF_RETAILER)
         print('                   Costs Listing')
         print('=============================================================')
-        print('Setup cost                       : ',self.SETUP_COST)
-        print('Holding cost                     : ',self.HOLDING_COST)
-        print('Purchasing cost                  : ',self.HOLDING_COST)
-        print('Shipping cost                    : ',self.SHIPPING_COST)
-        print('Lead time                        : ',self.LEAD_TIME)
-        print('Fixed Shipping cost              : ',self.FIXED_SHIPPING_COST)
+        print('Setup cost      : ',self.SETUP_COST)
+        print('Holding cost    : ',self.HOLDING_COST)
+        print('Purchasing cost : ',self.HOLDING_COST)
+        print('Shipping cost   : ',self.SHIPPING_COST)
+        print('Lead time       : ',self.LEAD_TIME)
+        print('Shipping cost   : ',self.FIXED_SHIPPING_COST)
         print('Retailer arrival rate priority   : ',self.RETAILER_ARRIVAL_RATE_1)
         print('Retailer arrival rate ordinary   : ',self.RETAILER_ARRIVAL_RATE_2)
-        print('Fixed cost                       : ',self.FIXED_COST)
-        print('Cost losing priority customer    : ',self.COST_LOSE_1)
-        print('Cost losing ordinary customer    : ',self.COST_LOSE_2)
-
-
+        print('Fixed cost      : ',self.FIXED_COST)
+        print('Cost losing priority customer : ',self.COST_LOSE_1)
+        print('Cost losing ordinary customer : ',self.COST_LOSE_2)
 
     def dcArrivalRate(self,X):
-        '''Find DC arrival rate based on retailer arrival rate
-           Arrival rate depends on X.
-           Chromosome -> X -> generate DC Arrival rate -> Compute total cost
+        '''
+        Find DC arrival rate based on retailer arrival rate
+        Arrival rate depends on X.
+        Chromosome --> X --> generate DC AR -- compute TC
         '''
         retailerArrivalRate1=np.reshape(self.RETAILER_ARRIVAL_RATE_1,(1,self.NO_OF_RETAILER))
         dcArrivalRate1=np.matmul(retailerArrivalRate1,np.array(X))
@@ -89,10 +84,6 @@ class SupplyChain:
         dcArrivalRate2=np.matmul(retailerArrivalRate2,np.array(X))
         self.DC_ARRIVAL_RATE_1=[ar for _,ar in enumerate(dcArrivalRate1)]
         self.DC_ARRIVAL_RATE_2=[ar for _,ar in enumerate(dcArrivalRate2)]
-      
-    def calcTerm1(self,Y:list):
-        '''Term 1: Calculate fixed cost of each DC'''  
-        return np.dot(self.FIXED_COST,Y)
     
     def calcProbState0(self,s:list,Q:list):
         '''Calculate probability at Stage 0'''
@@ -101,7 +92,7 @@ class SupplyChain:
             probState0List.append(self.DC_ARRIVAL_RATE_1[j]+self.DC_ARRIVAL_RATE_2[j])/(self.DC_ARRIVAL_RATE_1[j]+(self.DC_ARRIVAL_RATE_2[j]+(Q[j]*self.LEAD_TIME[j]))*pow((1+self.LEAD_TIME[j]/self.DC_ARRIVAL_RATE_1[j]),s[j]))
         return probState0List
     
-    def calcInventoryLevelCost(self,s:list,Q:list):#TODO
+    def calcInventoryLevelCost(self,s:list,Q:list):
         '''Calculate inventory level cost for each DC'''
         ilCostList=[]
         self.probState0List=self.calcProbState0(s,Q)
@@ -116,44 +107,13 @@ class SupplyChain:
             ilCostList.append(a*((b+c-d-e)*self.probState0List[j])+f*g*self.probState0List[j])
         return ilCostList
 
-    def calcTerm2(self,Y:list,s:list,Q:list):
-        '''Term 2: Calculate holding cost of each DC'''
-        totalHoldingCost=0
-        self.il=self.calcInventoryLevelCost(s,Q) 
-        for j in range(self.NO_OF_DC):
-            totalHoldingCost+=self.HOLDING_COST[j]*(self.il[j])*Y[j]
-        return totalHoldingCost
-        
     def calcReorderRate(self,s:list,Q:list):
         reorderCostList=[]
         probState0=self.calcProbState0(s,Q)
         for j in range(self.NO_OF_DC):
             reorderCostList.append(self.DC_ARRIVAL_RATE_1[j]+self.DC_ARRIVAL_RATE_2[j])*probState0[j]*(s[j]+1)
         return reorderCostList
-
-    def calcTerm3(self,Y:list,Q:list):
-        '''Term 3: Calculate setup cost, unit purchasing cost and fixed shipping cost per order between Supplier and DC'''
-        total=0
-        self.reorderCostList=self.calcReorderRate
-        for j in range(self.NO_OF_DC):
-            # Q_MIN_OF_J, REORDERRATE !
-            total+=(self.SETUP_COST[j]+self.FIXED_SHIPPING_COST[j]+(self.PURCHASING_COST_UNIT[j]*(Q[j])))*(self.reorderCostList[j])*Y[j]
-        return total
-
-    def calcTerm4(self,X:list):
-        '''Term 4: Transportation cost from DC to the retailer'''
-        total=0
-        for j in range(self.NO_OF_DC):
-            for i in range(self.NO_OF_RETAILER):
-                for r in range(len(self.CUSTOMER_CLASS.keys())):
-                    if(r==0):#Calculate priority arrival rate
-                        #MUST ENSURE ARRIVAL RATE IS FOR RETAILER NOT DC
-                        #DOUBLE CHECK SHIPPING COST IS MATRIX, MAKE SURE ROW N COL IS COLUMN
-                        total+=self.RETAILER_ARRIVAL_RATE_1[i]*self.SHIPPING_COST[i][j]*X[i][j]
-                    else: #Calculate ordinary arrival rate
-                        total+=self.RETAILER_ARRIVAL_RATE_2[i]*self.SHIPPING_COST[i][j]*X[i][j]
-        return total
-
+    
     def calcShortageRate1(self):
         '''Calculate Mean Shortage Rates for the PRIORITY customers at each DC'''
         shortageRate1List=[]
@@ -167,20 +127,44 @@ class SupplyChain:
         for j in range(self.NO_OF_DC):
             shortageRate2List.append(self.DC_ARRIVAL_RATE_2[j]*pow((1+self.LEAD_TIME[j]/self.DC_ARRIVAL_RATE_1[j]),s[j])*self.probState0List[j])
         return shortageRate2List
+    
+    def calcTerm1(self,Y:list):
+        '''Term 1: Calculate fixed cost of each DC
+            f[j] * Y[j]
+        '''  
+        return np.dot(self.FIXED_COST, Y)
+    
+    def calcTerm2(self,Y:list,s:list,Q:list):
+        '''Term 2: Calculate holding cost of each DC
+            h[j] * IL[j] * Y[j]
+        '''
+        self.il=self.calcInventoryLevelCost(s,Q)   # TODO: move to other place
+        return (np.dot(np.multiply(self.HOLDING_COST,self.il),Y))
+        
+    def calcTerm3(self,Y:list,Q:list):
+        '''Term 3: Calculate setup cost, unit purchasing cost and fixed shipping cost per order between Supplier and DC
+            (k[j] + g[j] + c[j] * Q[j]) * R[j] * Y[j]       
+        '''
+        self.reorderCostList=self.calcReorderRate  # TODO: move to other place
+        return np.dot(np.multiply(np.add(np.add(self.SETUP_COST,self.FIXED_SHIPPING_COST),np.multiply(self.PURCHASING_COST_UNIT,Q)),self.reorderCostList),Y)
+
+    def calcTerm4(self,X:list):
+        '''Term 4: Transportation cost from DC to the retailer
+            lambda[r][i] * C[i][j] * X[i][j]
+        '''
+        return np.sum(np.add(np.dot(self.RETAILER_ARRIVAL_RATE_1, np.multiply(self.SHIPPING_COST,X)),
+                             np.dot(self.RETAILER_ARRIVAL_RATE_2, np.multiply(self.SHIPPING_COST,X))))        
 
     def calcTerm5(self,Y:list):
-        totalLostCost=0
-        self.meanShortageRate1=self.calcShortageRate1()
-        self.meanShortageRate2=self.calcShortageRate2()
-        for j in range(self.NO_OF_DC):
-            for r in range(len(self.CUSTOMER_CLASS.keys())): 
-                if (r==0): #MSR for priority customer
-                    totalLostCost+=self.meanShortageRate1[j]*self.COST_LOSE_1*Y[j] 
-                else: #MSR for ordinary customer
-                    totalLostCost+=self.meanShortageRate2[j]*self.COST_LOSE_2*Y[j] 
-        return totalLostCost
+        '''Term 5: lost sales due to demand lost for each customer classes
+            SR[r][j] * LC[r] * Y[j]
+        '''
+        self.meanShortageRate1=self.calcShortageRate1()   # TODO: move to other place
+        self.meanShortageRate2=self.calcShortageRate2()   # TODO: move to other place
+        return np.dot(np.multiply(self.meanShortageRate1,self.COST_LOSE_1),Y) + np.dot(np.multiply(self.meanShortageRate2,self.COST_LOSE_2),Y)
 
-    def calcPenaltyCost(self,chromosome):
+    def penaltyCost(self,chromosome):
+        '''Calculate penalty cost for dummy DC'''
         '''Calculate penalty cost for dummy DC'''
         #Calc penalty cost based on how many retailer assign to dummy DC
         totalPenaltyCost=0
@@ -188,13 +172,21 @@ class SupplyChain:
         totalPenaltyCost=dummyDCCount*10000
         return totalPenaltyCost
     
+    #TODO check X or Y whether dummy DC is got open or not, if got add penalty cost
     def calcTotalCost(self,X:list,Y:list,s,Q):
         '''Calculate min total cost (TC)'''
-         #TODO arrival rate priority n ordinary rate
-        #self.DC_ARRIVAL_RATE_1,self.DC_ARRIVAL_RATE_2=self.dcArrivalRate() #TODO how to pass X?
+        #Find DC arrival rate based on retailer arrival rate. Arrival rate depends on X.
+        #Chromosome -> X -> generate DC Arrival rate -> Compute total cost
+        retailerArrivalRate1=np.reshape(self.RETAILER_ARRIVAL_RATE_1,(1,self.NO_OF_RETAILER))
+        dcArrivalRate1=np.matmul(retailerArrivalRate1,np.array(X))
+        retailerArrivalRate2=np.reshape(self.RETAILER_ARRIVAL_RATE_2,(1,self.NO_OF_RETAILER))
+        dcArrivalRate2=np.matmul(retailerArrivalRate2,np.array(X))
+        self.DC_ARRIVAL_RATE_1=[ar for _,ar in enumerate(dcArrivalRate1)]
+        self.DC_ARRIVAL_RATE_2=[ar for _,ar in enumerate(dcArrivalRate2)]
+
         return self.calcTerm1(Y)+self.calcTerm2(Y,s,Q)+self.calcTerm3()+self.calcTerm4()+self.calcTerm5()
     
-    #TODO Try to do total cost for one DC, no need to loop j
+    #Try to do total cost for one DC, no need to loop j
     def calcTotalCostForEachDC(self,X:list,Y,s,Q,j): #X is matrix, only pass that column(ONLY COL), Y only 1 or 0 not list d
         pass
 
@@ -207,6 +199,7 @@ class SupplyChain:
         return Qmin
     
 #Create SC instance objects
-if __name__=="__main__":
-    sc_small_II=SupplyChain('5-10','TYPE_II',display_param=True)
+if __name__ == "__main__":
+    sc_small_II = SupplyChain('5-10','TYPE_II', display_param=True)
+
 
